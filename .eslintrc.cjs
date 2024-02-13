a3d0bf3734082a0b010375afdc3656ea6ba83b39
QuadNard <lizardkingnardo@gmail.com>
2024-02-13 07:00:23 -0500

    build: dev deps

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+    project: true,
+  },
+  plugins: ["@typescript-eslint", "eslint-plugin-react"],
+   extends: [
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+  ],
+   ignorePatterns: ["node_modules", ".next", "out", "jest.config.js", "next.config.js", "fragment.glsl", "vertex.glsl", "gradient.tsx"],
+   rules: {
+
+   },
+};
+
+module.exports = config;
