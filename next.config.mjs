70bdf26469029c97a5d6d00be3e14b6a61bc6890
Justin Tulloch <101229476+justintulloch@users.noreply.github.com>
2025-09-15 05:13:00 +0000

    build: altering config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: { unoptimized: true },
   swcMinify: true,
         webpack: (config, { isServer }) => {
         config.module.rules.push({
